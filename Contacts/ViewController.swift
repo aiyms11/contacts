1354be5fa946e6b15dab020b2621fd966118038f
Madi Kabdrash <m.kabdrash@gmail.com>
2020-07-02 23:37:36 +0600

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Madi Kabdrash on 7/2/20.
+//  Copyright © 2020 Aiyms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
